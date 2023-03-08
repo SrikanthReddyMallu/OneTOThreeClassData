586e42da54be7656eacce1cf9f843509cff72e22
SrikanthReddyMallu <126140093+SrikanthReddyMallu@users.noreply.github.com>
2023-03-08 23:35:04 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,37 @@
+package com.three;
+
+public class Person 
+{
+private int id;
+private String name;
+private long phno;
+private Mobile mobile;
+
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public long getPhno() {
+	return phno;
+}
+public void setPhno(long phno) {
+	this.phno = phno;
+}
+public Mobile getMobile() {
+	return mobile;
+}
+public void setMobile(Mobile mobile) {
+	this.mobile = mobile;
+}
+
+
+}
